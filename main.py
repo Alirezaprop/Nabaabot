b7ea5dd605c6361ea8d64608ef24e0938856db66
Alirezaprop <Simcardiran@gmail.com>
2025-06-06 09:35:18 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler
 from telegram import Update
+from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام علیرضا جان! رباتت با موفقیت فعال شد 🎉")
+    await update.message.reply_text("سلام علیرضا جان! ربات با موفقیت راه افتاد 🚀")
 
-app = ApplicationBuilder().token(os.getenv("TELE_TOKEN")).build()
+app = ApplicationBuilder().token(os.getenv("TOKEN")).build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
